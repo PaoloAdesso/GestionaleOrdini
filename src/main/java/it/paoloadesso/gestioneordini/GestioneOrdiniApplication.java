0dcaaa786e0dfe0a245cd778e5b2bef3a01849a3
Paolo Adesso <paolo.adesso@linksmt.it>
2025-09-10 12:44:45 +0200

    First commit

---
+++ src/main/java/it/paoloadesso/gestioneordini/GestioneOrdiniApplication.java
@@ -0,0 +1,13 @@
+package it.paoloadesso.gestioneordini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneOrdiniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestioneOrdiniApplication.class, args);
+    }
+
+}
